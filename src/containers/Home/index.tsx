1ba14feaab7b61a4929d16bce5f9ea1b70192152
warmansuganda <warman.suganda@delosaqua.com>
2022-09-02 10:48:49 +0700

    feat: welcome to next.js

---
+++ src/containers/Home/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { HomeWrapper, Title } from './sytles';
+
+function Home() {
+  return (
+    <HomeWrapper>
+      <Title>Welcome to Next.js</Title>
+    </HomeWrapper>
+  );
+}
+
+export default Home;
---
+++ src/containers/Home/sytles.tsx
@@ -0,0 +1,11 @@
+import styled from '@emotion/styled';
+
+export const HomeWrapper = styled.div`
+  display: grid;
+  place-items: center;
+  height: 100vh;
+`;
+
+export const Title = styled.div`
+  font-size: 100px;
+`;
